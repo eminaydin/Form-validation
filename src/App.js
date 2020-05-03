92f596339b9fac5e8007c83b0e4e64c2fe3a29e5
Emin <eminaydin93@yahoo.com>
2020-05-03 11:26:07 +0200

    Component structure fixed

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 import FormInput from './components/FormInput';
 
 
+
 function App() {
-  const [validForm, setValidForm] = useState(false);
-
-  function childValue(params) {
-    if (!params) {
-      setValidForm(true)
-    }
-
-  }
   return (
     <div className="App">
-      {validForm ?
-        <p>sldkjfhsdkjfhsdkj</p>
-        :
-        <FormInput childValue={childValue} />
-      }
+
+      <FormInput />
+
 
     </div>
   );
